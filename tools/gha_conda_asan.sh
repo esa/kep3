a2744abfe417cc247aeaf3ecfa96b3e0a2c9cb04
Dario Izzo <dario.izzo@esa.int>
2023-07-21 10:34:20 +0200

    Frist CI

---
+++ tools/gha_conda_asan.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O mambaforge.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/mambaforge/bin:$PATH"
+bash mambaforge.sh -b -p $HOME/mambaforge
+mamba env create -f pykep3_devel.yml -q -p $deps_dir
+source activate $deps_dir
+
+export CXXFLAGS="$CXXFLAGS -fsanitize=address"
+
+mkdir build
+cd build
+
+cmake -G "Ninja" ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DCASCADE_BUILD_TESTS=yes -DBoost_NO_BOOST_CMAKE=ON
+
+cmake --build . -- -v
+
+ctest -j4 -VV
+
+
+
+set +e
+set +x
