951ddc6cffd99ddf8c1a0ff339dd5ac5d12a38d4
Dario Izzo <dario.izzo@gmail.com>
2023-07-22 17:54:34 +0200

    coverage

--- test/epoch_test.cpp
+++ test/epoch_test.cpp
@@ -19,7 +19,8 @@ using namespace kep3;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
 
-TEST_CASE("construct") {
+TEST_CASE("construct")
+{
   // test syntax
   REQUIRE_NOTHROW(epoch());
   REQUIRE_NOTHROW(epoch(123.456));
@@ -27,8 +28,15 @@ TEST_CASE("construct") {
   REQUIRE_NOTHROW(epoch(123.456, epoch::JD));
   REQUIRE_NOTHROW(epoch(123.456, epoch::MJD));
   REQUIRE_NOTHROW(epoch(31, 12, 2034));
+  // > 2000
   boost::posix_time::ptime posix_time_test(date(2034, 12, 31));
   REQUIRE_NOTHROW(epoch(posix_time_test));
+  REQUIRE(epoch(posix_time_test).get_posix_time() == posix_time_test);
+  // < 2000
+  boost::posix_time::ptime posix_time_test2(date(1980, 12, 31));
+  REQUIRE_NOTHROW(epoch(posix_time_test2));
+  REQUIRE(epoch(posix_time_test2).get_posix_time() == posix_time_test2);
+
   // test conversions
   REQUIRE(epoch(123.456).mjd2000() == epoch(123.456, epoch::MJD2000).mjd2000());
   REQUIRE(epoch(0.).mjd2000() == epoch(51544, epoch::MJD).mjd2000());
@@ -36,7 +44,8 @@ TEST_CASE("construct") {
   REQUIRE(epoch(31, 12, 2034).jd() == epoch(posix_time_test).jd());
 }
 
-TEST_CASE("epoch_operators") {
+TEST_CASE("epoch_operators")
+{
   REQUIRE(epoch(0.) == epoch(0.));
   REQUIRE(epoch(0.) != epoch(1.));
   REQUIRE(epoch(1.) > epoch(0.));
@@ -54,4 +63,18 @@ TEST_CASE("epoch_operators") {
   today = yesterday + 1;
   REQUIRE(today == epoch(0.));
   REQUIRE(today - yesterday == 1.);
+}
+
+TEST_CASE("conversions_from_string")
+{
+  {
+    std::string ts("20020131T000000");
+    epoch e(epoch_from_iso_string(ts));
+    REQUIRE(e == epoch(boost::posix_time::ptime(date(2002, 01, 31))));
+  }
+  {
+    std::string ts("2002-01-20 00:00:00.000");
+    epoch e(epoch_from_string(ts));
+    REQUIRE(e == epoch(boost::posix_time::ptime(date(2002, 01, 20))));
+  }
 }
