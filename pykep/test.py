eb0b93ceb46c0bb227c0ddaf34fa45d682499c9a
Dario Izzo <dario.izzo@gmail.com>
2023-10-01 11:18:04 +0200

    test suite and CI first attempt

--- pykep/test.py
+++ pykep/test.py
@@ -10,44 +10,44 @@
 import unittest as _ut
 
 
-def float_error(a: float, b: float):
+def float_abs_error(a: float, b: float):
     return abs(a - b)
 
 class anomaly_conversions_tests(_ut.TestCase):
     def test_m2e(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.m2e(pk.e2m(0.1, 0.1), 0.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.m2e(pk.e2m(0.1, 0.1), 0.1), 0.1) < 1e-14)
 
     def test_m2f(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.m2f(pk.f2m(0.1, 0.1), 0.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.m2f(pk.f2m(0.1, 0.1), 0.1), 0.1) < 1e-14)
 
     def test_f2e(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.f2e(pk.e2f(0.1, 0.1), 0.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.f2e(pk.e2f(0.1, 0.1), 0.1), 0.1) < 1e-14)
 
     def test_n2h(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.n2h(pk.h2n(0.1, 10.1), 10.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.n2h(pk.h2n(0.1, 10.1), 10.1), 0.1) < 1e-14)
 
     def test_n2f(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.n2f(pk.f2n(0.1, 10.1), 10.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.n2f(pk.f2n(0.1, 10.1), 10.1), 0.1) < 1e-14)
 
     def test_f2h(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.f2h(pk.h2f(0.1, 10.1), 10.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.f2h(pk.h2f(0.1, 10.1), 10.1), 0.1) < 1e-14)
 
     def test_f2zeta(self):
         import pykep as pk
 
-        self.assertTrue(float_error(pk.f2zeta(pk.zeta2f(0.1, 10.1), 10.1), 0.1) < 1e-14)
+        self.assertTrue(float_abs_error(pk.f2zeta(pk.zeta2f(0.1, 10.1), 10.1), 0.1) < 1e-14)
 
 
 def run_test_suite():
---
+++ tools/gha_linux_pykep.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install wget
+
+# Install conda+deps.
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh -O mambaforge.sh
+export deps_dir=$HOME/local
+export PATH="$HOME/mambaforge/bin:$PATH"
+bash mambaforge.sh -b -p $HOME/mambaforge
+mamba env create -f kep3_devel.yml -q -p $deps_dir
+source activate $deps_dir
+
+# First we build and install the kep3 library
+mkdir build
+cd build
+cmake -G "Ninja" ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -Dkep3_BUILD_TESTS=no -Dkep3_BUILD_Benchmarks=no -Dkep3_BUILD_PYTHON_BINDINGS=no -DBoost_NO_BOOST_CMAKE=ON
+make VERBOSE=1 install
+
+# Then we build and install pykep
+cd ..
+mkdir build_pykep
+cmake -G "Ninja" ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -Dkep3_BUILD_TESTS=no -Dkep3_BUILD_Benchmarks=no -Dkep3_BUILD_PYTHON_BINDINGS=yes -DBoost_NO_BOOST_CMAKE=ON
+make VERBOSE=1 install
+
+python -c "import pykep.test; pykep.test.run_test_suite()"
+
+set +e
+set +x
