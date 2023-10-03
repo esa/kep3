ec681eea94b4b2388f68b6785f877d426565f554
Dario Izzo <dario.izzo@gmail.com>
2023-10-03 22:22:10 +0200

    gh-pages upload

--- tools/gha_linux_pykep.sh
+++ tools/gha_linux_pykep.sh
@@ -31,5 +31,9 @@ cmake --build . --target=install --config=Release -- -j 2
 
 python -c "import pykep.test; pykep.test.run_test_suite()"
 
+# Build the documentation.
+cd ${GITHUB_WORKSPACE}/doc
+make html linkcheck
+
 set +e
 set +x
