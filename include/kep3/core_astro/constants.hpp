091e69b4d42a285732a6a2d8ef47bed25f84857d
Francesco Biscani <bluescarni@gmail.com>
2023-10-04 22:23:51 +0200

    Some more small fixes/cleanups.

--- include/kep3/core_astro/constants.hpp
+++ include/kep3/core_astro/constants.hpp
@@ -10,8 +10,6 @@
 #ifndef kep3_CONSTANTS_H
 #define kep3_CONSTANTS_H
 
-#include <cmath>
-
 #include <boost/math/constants/constants.hpp>
 
 namespace kep3
@@ -25,22 +23,22 @@ enum elements_type {
     POSVEL, // position and Velocity
 };
 
-constexpr double pi = boost::math::constants::pi<double>();
-constexpr double half_pi = boost::math::constants::half_pi<double>();
+inline constexpr double pi = boost::math::constants::pi<double>();
+inline constexpr double half_pi = boost::math::constants::half_pi<double>();
 
-constexpr double AU = 149597870700.0;                 // Astronomical Unit (m)
-constexpr double CAVENDISH = 73.6687e-11;             // Cavendish constant (N M^2 / kg^2)
-constexpr double MU_SUN = 1.32712440018e20;           // Sun's gravitational parameter (m^3/s^2 kg)
-constexpr double MU_EARTH = 398600441800000.0;        // Earth's gravitational parameter (m^3/s^2 kg)
-constexpr double EARTH_VELOCITY = 29784.691831696804; // Earth's velocity. (m/s)
-constexpr double EARTH_J2 = 1.08262668E-03;
-constexpr double EARTH_RADIUS = 6378137; // Earth's radius (m)
-constexpr double DEG2RAD = (pi / 180.0);
-constexpr double RAD2DEG = (180.0 / pi);
-constexpr double DAY2SEC = 86400.0;
-constexpr double SEC2DAY = (1. / DAY2SEC);
-constexpr double DAY2YEAR = (1. / 365.25);
-constexpr double G0 = 9.80665; // Acceleration at Earth's surface (m/s^2)
+inline constexpr double AU = 149597870700.0;                 // Astronomical Unit (m)
+inline constexpr double CAVENDISH = 73.6687e-11;             // Cavendish constant (N M^2 / kg^2)
+inline constexpr double MU_SUN = 1.32712440018e20;           // Sun's gravitational parameter (m^3/s^2 kg)
+inline constexpr double MU_EARTH = 398600441800000.0;        // Earth's gravitational parameter (m^3/s^2 kg)
+inline constexpr double EARTH_VELOCITY = 29784.691831696804; // Earth's velocity. (m/s)
+inline constexpr double EARTH_J2 = 1.08262668E-03;
+inline constexpr double EARTH_RADIUS = 6378137; // Earth's radius (m)
+inline constexpr double DEG2RAD = (pi / 180.0);
+inline constexpr double RAD2DEG = (180.0 / pi);
+inline constexpr double DAY2SEC = 86400.0;
+inline constexpr double SEC2DAY = (1. / DAY2SEC);
+inline constexpr double DAY2YEAR = (1. / 365.25);
+inline constexpr double G0 = 9.80665; // Acceleration at Earth's surface (m/s^2)
 
 } // namespace kep3
 
