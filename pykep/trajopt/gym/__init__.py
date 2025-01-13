7b26bcfed8f270af4a28835f52000c110428e46e
Dario Izzo <dario.izzo@gmail.com>
2025-01-13 15:10:29 +0100

    added gym problems rosetta and eve

--- pykep/trajopt/gym/__init__.py
+++ pykep/trajopt/gym/__init__.py
@@ -1,2 +1,4 @@
 from ._cassini_mga import cassini1, cassini1_a, cassini1_n
 from ._cassini_mga_1dsm import cassini2
+from ._eve_mga_1dsm import eve_mga1dsm, eve_mga1dsm_a, eve_mga1dsm_n    
+from ._rosetta_mga_1dsm import rosetta
