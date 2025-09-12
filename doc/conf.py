ded27932c02d6ddf73eabbd9c87543e523d34dd1
Dario Izzo <dario.izzo@gmail.com>
2025-09-12 11:53:11 +0200

    fix the linkchecker issue wikth ESA filters, 2nd trial

--- doc/conf.py
+++ doc/conf.py
@@ -91,7 +91,7 @@ myst_enable_extensions = [
 
 
 # Make the linkcheck (and other HTTP requests) look like a real browser
-user_agent = (
+linkcheck_user_agent = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
     "AppleWebKit/537.36 (KHTML, like Gecko) "
     "Chrome/122.0.0.0 Safari/537.36"
